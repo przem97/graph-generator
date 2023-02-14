837de56ea4087268688f66460f2a780a280630b6
przem97 <przemyslaw.przybysz97@gmail.com>
2023-02-14 23:14:17 +0100

    handle request body params for initializer

--- src/routes/initializer.ts
+++ src/routes/initializer.ts
@@ -6,13 +6,15 @@ import IGraphInitializer from '../services/graphInitializerInterface'
 const router: Router = express.Router()
 
 router.get('/initialize', (req: Request, res: Response) => {
-    console.log(req.body)
-
     res.setHeader('Content-Type', 'application/json');
     
     let graphInitializer: IGraphInitializer = new GraphInitializer();
 
-    const components: Array<Component> = graphInitializer.initializeGraph(10, 10, 3)
+    let totalVertices = req.body.totalVertices ? req.body.totalVertices : 0;
+    let totalEdges = req.body.totalEdges ? req.body.totalEdges : 0;
+    let totalComponents = req.body.totalComponents ? req.body.totalComponents : 0;
+
+    const components: Array<Component> = graphInitializer.initializeGraph(totalVertices, totalEdges, totalComponents)
 
     res.send({ "graph": {
         "components": components
