bd36170b8ec731585ac313e918859130ba4fb257
przem97 <przemyslaw.przybysz97@gmail.com>
2023-08-31 22:23:45 +0200

    openapi merger description

--- src/routes/merger.ts
+++ src/routes/merger.ts
@@ -5,30 +5,64 @@ import ComponentMerger from '../utils/component/merger/componentMerger';
 
 const router: Router = express.Router();
 
+/**
+ * @openapi
+ * /graphs/merge:
+ *   post:
+ *     description: Merge components within the graph with speficied edge
+ *     tags:
+ *       - MergeAPI
+ *     requestBody:
+ *       description: The graph object
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstComponent:
+ *                 $ref: '#components/schemas/component'
+ *               secondComponent:
+ *                 $ref: '#components/schemas/component'
+ *               mergingEdge:
+ *                 $ref: '#components/schemas/edge'
+ *     response:
+ *       200:
+ *         description: The graph object with merged components
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 graph:
+ *                   $ref: '#components/schemas/graph'
+ *       202:
+ *         description: Components cannot be merged
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Cannot merge components
+ */
 router.post('/merge', (req: Request, res: Response) => {
-    try {
-        const graph = req.body.graph;
+    const component1: Component =  new Component();
+    const component2: Component =  new Component();
+    const mergingEdge: Edge = new Edge(0, 0, 0);
+    Object.assign(component1, req.body.firstComponent);
+    Object.assign(component2, req.body.secondComponent);
+    Object.assign(mergingEdge, req.body.mergingEdge);
 
-        const component1: Component =  new Component();
-        const component2: Component =  new Component();
-        const mergingEdge: Edge = new Edge(0, 0, 0);
-        Object.assign(component1, graph.components[0]);
-        Object.assign(component2, graph.components[1]);
-        Object.assign(mergingEdge, req.body.mergingEdge);
-    
-        const mergedComponent: Component = new ComponentMerger().merge(component1, component2, mergingEdge);
-    
-        if (mergedComponent.isEmpty()) {
-            res.status(202).send({ "message": "cannot merge components!"});   
-        } else {
-            res.send({ "graph": {
-                "component": mergedComponent
-            }});
-        }
-    } catch (err) {
-        res.status(500).send({
-            "message": "internal server error. check logs."
-        });
+    const mergedComponent: Component = new ComponentMerger().merge(component1, component2, mergingEdge);
+
+    if (mergedComponent.isEmpty()) {
+        res.status(202).send({ "message": "Cannot merge components"});   
+    } else {
+        res.send({ "graph": {
+            "components": mergedComponent
+        }});
     }
 });
 
