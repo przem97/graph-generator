da195dec2a4a78572df538e29757e586e5a93e88
przem97 <przemyslaw.przybysz97@gmail.com>
2023-08-31 11:20:27 +0200

    openapi splitter description

--- src/routes/splitter.ts
+++ src/routes/splitter.ts
@@ -4,26 +4,58 @@ import ComponentSplitter from '../utils/component/splitter/componentSplitter';
 
 const router: Router = express.Router();
 
+/**
+ * @openapi
+ * /graphs/split:
+ *   post:
+ *     description: Given the graph find the components which can be splitted and split them into smaller ones
+ *     tags:
+ *       - SplitAPI
+ *     requestBody:
+ *       description: The graph object
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               graph:
+ *                 $ref: '#components/schemas/graph'
+ *     responses:
+ *       200:
+ *         description: The graph object with splitted components
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 graph:
+ *                   $ref: '#components/schemas/graph'
+ *       202:
+ *         description: No components to split
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: No components to split
+ */
 router.post('/split', (req: Request, res: Response) => {
-    try {
-        const graph = req.body.graph;
+    const graph = req.body.graph;
 
-        const component: Component =  new Component();
-        Object.assign(component, graph.components[0]);
-    
-        const components: Component[] = new ComponentSplitter().split(component);
-    
-        if (components.length === 1) {
-            res.status(202).send({ "message": "no components to split!"});   
-        } else {
-            res.send({ "graph": {
-                "components": components
-            }});
-        }
-    } catch (err) {
-        res.status(500).send({
-            "message": "internal server error. check logs."
-        });
+    const component: Component =  new Component();
+    Object.assign(component, graph.components[0]);
+
+    const components: Component[] = new ComponentSplitter().split(component);
+
+    if (components.length === 1) {
+        res.status(202).send({ "message": "No components to split"});   
+    } else {
+        res.send({ "graph": {
+            "components": components
+        }});
     }
 });
 
